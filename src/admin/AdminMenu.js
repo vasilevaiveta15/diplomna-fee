c7be4876c141c1710d50e71475c337721515cb0d
Iveta Vasileva <vasilevaiveta15@gmail.com>
2024-09-08 21:49:19 +0300

    added service for next year

--- src/admin/AdminMenu.js
+++ src/admin/AdminMenu.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Typography, Button } from '@mui/material';
+import { Box, Typography, Button, Snackbar } from '@mui/material';
 import StudentRegistration from './StudentRegistration'; // Импортирайте компонента за регистрация на ученик
 import Term from './Term'; // Импортирайте компонента Term
 import FinalGrade from './FinalGrade'; // Импортирайте новия компонент FinalGrade
@@ -8,6 +8,7 @@ const AdminMenu = () => {
   const [showStudentRegistration, setShowStudentRegistration] = useState(false);
   const [showTerm, setShowTerm] = useState(false);
   const [showFinalGrade, setShowFinalGrade] = useState(false); // Добавяме състояние за FinalGrade
+  const [snackbarOpen, setSnackbarOpen] = useState(false); // Състояние за Snackbar
 
   const handleShowStudentRegistration = () => {
     setShowStudentRegistration(true);
@@ -35,6 +36,29 @@ const AdminMenu = () => {
 
   const handleTermSuccess = () => {
     setShowTerm(false);
+  };
+
+  const handleAddNextYear = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/be/e-journal/next-year', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem('authToken')}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Неуспешно добавяне на нова година');
+      }
+
+      setSnackbarOpen(true); // Отваря Snackbar при успешен отговор
+
+      // Скрие съобщението след 4 секунди
+      setTimeout(() => setSnackbarOpen(false), 4000);
+    } catch (error) {
+      alert(error.message);
+    }
   };
 
   return (
@@ -78,8 +102,19 @@ const AdminMenu = () => {
               <Button variant="contained" color="primary" sx={{ marginTop: 2 }} onClick={handleShowFinalGrade}>
                 Добавяне на срочна оценка
               </Button>
+              <Button variant="contained" color="secondary" sx={{ marginTop: 2 }} onClick={handleAddNextYear}>
+                Добавяне на нова година
+              </Button>
             </Box>
         )}
+        <Snackbar
+            open={snackbarOpen}
+            autoHideDuration={4000}
+            message="Успешно добавихте нова година на всички ученици!"
+            ContentProps={{
+              sx: { backgroundColor: 'green', color: 'white', textAlign: 'center' },
+            }}
+        />
       </Box>
   );
 };
