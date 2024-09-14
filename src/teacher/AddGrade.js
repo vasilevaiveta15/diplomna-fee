265ebe1b8c33de468a8d6cdd3fb11432a4ca8ff0
Iveta Vasileva <vasilevaiveta15@gmail.com>
2024-09-14 17:24:24 +0300

    added term when add grade

--- src/teacher/AddGrade.js
+++ src/teacher/AddGrade.js
@@ -1,4 +1,3 @@
-// src/components/AddGrade.js
 import React, { useState, useEffect } from 'react';
 import './AddGrade.css'; // Импортиране на CSS файла за стилизация
 
@@ -6,6 +5,7 @@ const AddGrade = ({ onBack }) => {
   const [studentId, setStudentId] = useState('');
   const [grade, setGrade] = useState('');
   const [subjectId, setSubjectId] = useState(null);
+  const [term, setTerm] = useState('1'); // Дефинираме състоянието за срока (по подразбиране "1")
 
   useEffect(() => {
     const fetchTeacherProfile = async () => {
@@ -42,6 +42,7 @@ const AddGrade = ({ onBack }) => {
           studentId,
           grade,
           subjectId,
+          term, // Добавяме избраната стойност на срока към заявката
         }),
       });
 
@@ -58,29 +59,39 @@ const AddGrade = ({ onBack }) => {
   };
 
   return (
-    <div className="form-wrapperrr">
-      <div className="form-containerrr">
-        <h2>Добавяне на оценка</h2>
-        <label>
-          Номер на ученик:
-          <input
-            type="text"
-            value={studentId}
-            onChange={(e) => setStudentId(e.target.value)}
-          />
-        </label>
-        <label>
-          Оценка:
-          <input
-            type="text"
-            value={grade}
-            onChange={(e) => setGrade(e.target.value)}
-          />
-        </label>
-        <button onClick={handleAddGrade}>Добави оценка</button>
-        <button onClick={onBack} className="back-button">Назад</button>
+      <div className="form-wrapperrr">
+        <div className="form-containerrr">
+          <h2>Добавяне на оценка</h2>
+          <label>
+            Номер на ученик:
+            <input
+                type="text"
+                value={studentId}
+                onChange={(e) => setStudentId(e.target.value)}
+            />
+          </label>
+          <label>
+            Оценка:
+            <input
+                type="text"
+                value={grade}
+                onChange={(e) => setGrade(e.target.value)}
+            />
+          </label>
+
+          {/* Добавяне на бутон за избор на срок */}
+          <label>
+            Срок:
+            <select value={term} onChange={(e) => setTerm(e.target.value)}>
+              <option value="1">1ви Срок</option>
+              <option value="2">2ри Срок</option>
+            </select>
+          </label>
+
+          <button onClick={handleAddGrade}>Добави оценка</button>
+          <button onClick={onBack} className="back-button">Назад</button>
+        </div>
       </div>
-    </div>
   );
 };
 
